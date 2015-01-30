fd2e062d4e7136d43c12225a8fa5b4df3476d111
sonnym <michaud.sonny@gmail.com>
2015-01-29 19:41:21 -0500

    add canvas area

--- Doodle/View.elm
+++ Doodle/View.elm
@@ -3,13 +3,35 @@ module Doodle.View where
 import List
 
 import Color (..)
+import Graphics.Collage (..)
 import Graphics.Element (..)
 
 swathSize = 25
 palettePadding = 5
 
 view : Element
-view = flow right (List.intersperse (spacer palettePadding swathSize) (List.map swath colors))
+view = flow down [palette, (spacer 0 10), canvas]
+
+palette : Element
+palette =
+  flow right
+    (List.intersperse (spacer palettePadding swathSize)
+      (List.map swath colors))
+
+canvas : Element
+canvas =
+  let
+    colorCount = List.length colors
+
+    width = ((colorCount - 1) * palettePadding) + (colorCount * swathSize)
+    height = (width * 9 // 16)
+
+    floatWidth = toFloat width
+    floatHeight = toFloat height
+  in
+    collage width height
+      [rect floatWidth floatHeight
+        |> outlined (solid black)]
 
 swath : Color -> Element
 swath c = color c (spacer swathSize swathSize)
