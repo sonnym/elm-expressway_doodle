e99f0304148e6357337a4653b0feab4c6f7f3e6d
sonnym <michaud.sonny@gmail.com>
2015-01-29 23:48:53 -0500

    fix name of swatches

--- Doodle/View.elm
+++ Doodle/View.elm
@@ -6,7 +6,7 @@ import Color (..)
 import Graphics.Collage (..)
 import Graphics.Element (..)
 
-swathSize = 50
+swatchSize = 50
 palettePadding = 10
 
 view : Element
@@ -15,15 +15,15 @@ view = flow down [palette, (spacer 0 10), canvas]
 palette : Element
 palette =
   flow right
-    (List.intersperse (spacer palettePadding swathSize)
-      (List.map swath colors))
+    (List.intersperse (spacer palettePadding swatchSize)
+      (List.map swatch colors))
 
 canvas : Element
 canvas =
   let
     colorCount = List.length colors
 
-    width = ((colorCount - 1) * palettePadding) + (colorCount * swathSize)
+    width = ((colorCount - 1) * palettePadding) + (colorCount * swatchSize)
     height = (width * 9 // 16)
 
     floatWidth = toFloat width
@@ -33,13 +33,13 @@ canvas =
       [rect floatWidth floatHeight
         |> outlined (solid black)]
 
-swath : Color -> Element
-swath color =
+swatch : Color -> Element
+swatch color =
   let
-    filledSquare = filled color (square (swathSize - 1))
-    border = outlined (solid charcoal) (square swathSize)
+    filledSquare = filled color (square (swatchSize - 1))
+    border = outlined (solid charcoal) (square swatchSize)
   in
-    collage swathSize swathSize [filledSquare, border]
+    collage swatchSize swatchSize [filledSquare, border]
 
 colors : List Color
 colors =
