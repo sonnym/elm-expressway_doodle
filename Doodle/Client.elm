79917b7870298ef2df340c6d52082013897bf008
sonnym <michaud.sonny@gmail.com>
2015-02-21 11:55:09 -0500

    pass signal of grid through ports

--- Doodle/Client.elm
+++ Doodle/Client.elm
@@ -2,14 +2,17 @@ module Doodle.Client where
 
 import Signal
 
+import Color (Color)
 import Graphics.Element (Element)
 
+import Doodle.Model (Grid)
+import Doodle.State (canvasUpdates)
 import Doodle.Display (display)
 
 main : Signal Element
 main = display
 
-port receiveState : Signal String
+port receiveState : Signal Grid
 
-port sendInput : Signal Int
-port sendInput = Signal.map2 (+) Mouse.x Mouse.y
+port sendInput : Signal ((Int, Int), Color)
+port sendInput = canvasUpdates
--- Doodle/Server.elm
+++ Doodle/Server.elm
@@ -1,12 +1,16 @@
 module Doodle.Server where
 
+import Color (Color)
 import Signal
+
+import Doodle.Model (Grid)
+import Doodle.State (state)
 
 -- Boilerplate
 import Text
 main = Text.asText "main"
 
-port receiveInput : Signal Int
+port receiveInput : Signal ((Int, Int), Color)
 
-port sendState : Signal String
-port sendState = Signal.map toString receiveInput
+port sendState : Signal Grid
+port sendState = state receiveInput
